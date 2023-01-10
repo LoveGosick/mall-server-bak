9007665377fb6c8dd591fc65e95e32b2f2f3b4dd
huangyihang <huangyihang@gmail.com>
2023-01-10 15:58:06 +0800

    初始化项目

--- src/main/java/com/example/mall/Launcher.java
+++ src/main/java/com/example/mall/Launcher.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MallServerApplication {
+public class Launcher {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MallServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Launcher.class, args);
+  }
 
 }
