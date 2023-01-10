d125ba0ef77ff28a486798c97d3687f724a304a9
huangyihang <huangyihang@gmail.com>
2023-01-10 16:35:07 +0800

    SpringBoot集成Knife4j

---
+++ mall-server-config/src/main/java/com/example/mall/config/knife4j/Knife4jConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.mall.config.knife4j;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * Knife4j在线API文档配置
+ *
+ * @author huangyihang
+ * @version 1.0
+ * @date 2023-01-10
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfiguration {
+
+  @Bean(value = "dockerBean")
+  public Docket dockerBean() {
+    //指定使用Swagger2规范
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(new ApiInfoBuilder()
+            //描述字段支持Markdown语法
+            .description("# Knife4j RESTful APIs")
+            .termsOfServiceUrl("https://doc.xiaominfo.com/")
+            .contact("xiaoymin@foxmail.com")
+            .version("1.0")
+            .build())
+        //分组名称
+        .groupName("用户服务")
+        .select()
+        //这里指定Controller扫描包路径
+        .apis(RequestHandlerSelectors.basePackage("com.example.mall.web.controller"))
+        .paths(PathSelectors.any())
+        .build();
+  }
+}
